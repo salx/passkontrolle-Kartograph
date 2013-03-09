873c1ad4f85ec542a671f8f8458e50ddadc5ffab
Alexandra Siebenhofer <salx@0xx0.net>
2013-03-09 17:27:53 +0100

    changed the restrictions variable

--- src/Kartograph.js
+++ src/Kartograph.js
@@ -11,8 +11,11 @@ var map = //muss ich hier ein svg definieren? mir ist an der stelle der Zeit-cod
 var colorCodes = [#aaa, #bbb, #ccc]; //sifu: was spricht dagegen, 
 //hier auch eine funktion mit if-statement zu machen (siehe ZEIT)
 
+//define a variable containing the data about the travel restrictions
+var restrictions = //enter the data....
+
 //the function, where all the action happens		
-function map(map){ //sifu: what di I need to hand over? map? some container in the DOM, right?
+function map(restrictions){ //sifu: how about I hand over the restrictions variable here?
 
 	//loadMap svg to the map instance
 	map.loadMap( 'some.svg' function(){
@@ -23,16 +26,16 @@ function map(map){ //sifu: what di I need to hand over? map? some container in t
 			}
 	});
 	
-	//load data into an object (containing objects wit key-value pairs)
-	var restrictions = //loading the data....see data-import-test
-
 	//define a reColor method(data)
-		//me.currentRestriction = restrictions[ data.identifier ]
-		//call getLayer('countries').style( fill, setColor )
+		currentRestriction = restrictions[ data.identifier ]
+		map.getLayer('countries').style( fill, setColor )
 
 	//define a setColor method(data)
 		//variable =  value of data.identifier ad color
 		//return colors [restriction-1]
+		setColor('countries'){
+			
+		}
 
 	//define an on-click event
 		map.addLayer('countries' {
